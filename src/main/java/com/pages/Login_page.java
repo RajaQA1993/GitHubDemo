ce84ca3f9778173f89adce5baaa07dea9b8b61f1
Raja Reddy <User@User-pC>
2021-03-28 08:21:14 +0530

    First commit

---
+++ src/main/java/com/pages/Login_page.java
@@ -0,0 +1,13 @@
+package com.pages;
+
+public class Login_page {
+	
+	
+	
+	public void loginpage() {
+		
+		System.out.println("This is Login page");
+	}
+	
+	
+}
---
+++ src/test/java/com/Test/Login_Test.java
@@ -0,0 +1,13 @@
+package com.Test;
+
+public class Login_Test{
+	
+	
+	
+	
+public void logintest() {
+		
+		System.out.println("This is Login Test");
+	}
+
+}
